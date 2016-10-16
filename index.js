588558de7e87a219a02c91e99e6ab89dbf17eb3f
zhanghao <jsyczhanghao@sina.com>
2016-10-16 20:53:43 +0800

    解决blade 标签前面有字符，并且压缩时报错的bug

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ module.exports = function(ret, conf, setting, opt){
         }
 
         lothar.util.map(ret.src, function(subpath, file){
-            if(file.isWidget && file.isHtmlLike){
-                var content = file.getContent();
-                
-                if(lothar.isPreviewMode){
+            if(file.isHtmlLike){
+                var content = file.getContent().replace(/<<<BLADE_LABEL_HACK>>>/g, ' ');
+
+                if(file.isWidget && lothar.isPreviewMode){
                     content = "@if(!isset($__isRef))\n@include('common._static_', ['type' => 'head'])\n@endif\n" + content + "\n@if(!isset($__isRef))\n@include('common._static_', ['type' => 'bottom'])\n@endif\n";
                 }
 
